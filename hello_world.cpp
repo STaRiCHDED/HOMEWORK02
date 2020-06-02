8899416fcdbf9dcd292fdd6506208a5b09a0e41d
STaRiCHDED <nik179804@gmail.com>
2020-06-02 22:30:32 +0300

    added1 hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,8 @@
-#include <iosrteam>
+#include <iosrteam> //Создаём программу
 int main()
 {
-std::string name;
-getline(cin, name);
-std::cout << "Hello world from " << name << std::endl;
-return 0;
-} 
+    std::string name;
+    getline(cin, name);
+    std::cout << "Hello world from " << name << std::endl;
+    return 0;
+} //Программа завершила работу

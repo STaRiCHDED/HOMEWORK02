e4171de6c0ba2acc5335f1ec170d4e90931ae3c4
STaRiCHDED <61161128+STaRiCHDED@users.noreply.github.com>
2020-06-03 01:16:51 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,8 @@
 #include <iosrteam> //Создаём программу
 int main()
 {
-    в std::string name;
-    в getline(cin, name);
-    в std::cout << "Hello world from " << name << std::endl;
+    std::string name;
+    getline(cin, name);
+    std::cout << "Hello world from " << name << std::endl;
     return 0;
 } //Программа завершила работу
